0867f508ab5c27159a89dcc76aa13e1d60ca2e4e
SIVASANTHI R <sivasanthi.250425@cse.ritchennai.edu.in>
2026-09-22 23:21:27 +0530

    java3

---
+++ Main.java
@@ -0,0 +1,40 @@
+
+
+public class Main {
+	public static void main(String [] arg) {
+		String name="raja";
+		char A='A';
+		int n=100;
+		double d=9800.983862;
+		boolean adult=true;
+		double d1=-2300.7423;
+		int n1=12;
+				int n2=1234;
+				int n3=1234;
+				
+		
+		if (adult) {
+		System.out.printf("hello %s scored %c grade and %d marks in maths with overall percentage of %.2f\n",name,A,n,d);
+		
+		}
+		System.out.printf("%+.2f\n",d);
+		System.out.printf("%,.2f\n",d);
+		System.out.printf("% (.2f\n",d1);
+		System.out.printf("% .2f\n",d);
+		System.out.printf("% .2f\n",d1)
+		;
+		System.out.printf("%6d\n",n1);
+		System.out.printf("%02d\n",n2);
+		System.out.printf("%06d\n",n3);
+		System.out.printf("%4d\n",n1);
+		System.out.printf("%4d\n",n2);
+		System.out.printf("%4d\n",n3);
+		System.out.printf("%-3d\n",n1);
+		System.out.printf("%-9d\n",n2);
+		System.out.printf("%-4d\n",n3);
+		
+
+		
+		
+	}
+}
